68a46ba63efa9f1bc75d8ac2eff90d68ce473ed2
Cindy <zxx.njc@gmail.com>
2020-10-09 02:15:48 +0800

    Sign up and login screens

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="vue-template">
+    <!-- Navigation -->
+    <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
+      <div class="container">
+        <p class="navbar-brand float-left pb-0">
+          Disrupting the Bank
+        </p>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-primary" to="/">Sign up</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Main -->
+    <div class="App">
+      <div class="vertical-center">
+        <div class="inner-block">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
